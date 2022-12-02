4efeed4e16e35c59e94f092c591a4e409bd14e64
roman avramenko <116434645+4remich@users.noreply.github.com>
2022-12-02 17:15:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def get_message():
 @app.route('/')
 def main():
     bot.remove_webhook()
-    bot.set_webhook(url='***' + TOKEN)
+    bot.set_webhook(url='https://radiation-rate.herokuapp.com/' + TOKEN)
     return "Python Telegram Bot", 200
 
 
